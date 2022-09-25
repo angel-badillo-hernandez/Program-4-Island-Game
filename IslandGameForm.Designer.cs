415e6b472f7bc1e7100609b8970bd5a0af46f148
Angel Badillo <badilloa022402@gmail.com>
2022-09-25 11:39:29 -0500

    working on Form design

--- IslandGameForm.Designer.cs
+++ IslandGameForm.Designer.cs
@@ -28,12 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.InfoButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(201, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(348, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Find the Tropical Island";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // InfoButton
+            // 
+            this.InfoButton.Location = new System.Drawing.Point(555, 9);
+            this.InfoButton.Name = "InfoButton";
+            this.InfoButton.Size = new System.Drawing.Size(52, 30);
+            this.InfoButton.TabIndex = 1;
+            this.InfoButton.Text = "Help";
+            this.InfoButton.UseVisualStyleBackColor = true;
+            this.InfoButton.Click += new System.EventHandler(this.HelpButton_Click);
+            // 
+            // IslandGameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.InfoButton);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "IslandGameForm";
+            this.Text = "Find the Tropical Island";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Button InfoButton;
     }
 }
--- IslandGameForm.cs
+++ IslandGameForm.cs
@@ -3,11 +3,20 @@ namespace program4
 {
     public partial class IslandGameForm : Form
     {
+        NavigationSystem navigationSystem;
         public IslandGameForm()
         {
-            NavigationSystem navigationSystem;
-
             InitializeComponent();
+        }
+
+        private void HelpButton_Click(object sender, EventArgs e)
+        {
+            string text = "To start the game, enter the dimensions of the " +
+                "map.\nThe map cannot be 0 x 0 or smaller, nor " +
+                "greater than 10 x 10.\n\nOnce the size is entered,";
+            string caption = "How to Play Find the Tropical Island";
+            
+            MessageBox.Show(text, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
