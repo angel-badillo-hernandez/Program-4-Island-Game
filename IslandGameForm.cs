8e7bccaa5717898acae04d674842768bfde06900
Angel Badillo <badilloa022402@gmail.com>
2022-09-25 12:44:17 -0500

    changes

--- IslandGameForm.cs
+++ IslandGameForm.cs
@@ -12,10 +12,11 @@ namespace program4
 
         private void HelpButton_Click(object sender, EventArgs e)
         {
+            string caption = "How to Play Find the Tropical Island";
+
             string text = "To start the game, enter the dimensions of the " +
                 "map.\nThe map cannot be 0 x 0 or smaller, nor " +
                 "greater than 10 x 10.\n\nOnce the size is entered, you can begin entering guesses";
-            string caption = "How to Play Find the Tropical Island";
             
             MessageBox.Show(text, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
